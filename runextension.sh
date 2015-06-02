47213c86a431766d8f7c1d56ed4e1723e38d4aec
asanka sanjaya <angal.sh@gmail.com>
2015-06-02 17:29:49 +0000

    adding build function

--- runextension.sh
+++ runextension.sh
@@ -1,6 +1,12 @@
 
 #buiuld and run gce-extension
 cd
+
+cd 
+cd stratos-source/extensions/load-balancer/gce-extension/
+mvn clean install
+
+cd
 rm -R gce-extension/*
 cp /home/sanjaya/stratos-source/extensions/load-balancer/gce-extension/target/org.apache.stratos.gce.extension-4.1.0-SNAPSHOT.zip gce-extension/
 
