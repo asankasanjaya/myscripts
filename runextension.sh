192fbf66d2c54d9b29d48a450d9cdaa6f467399c
asanka sanjaya <angal.sh@gmail.com>
2015-06-02 18:11:58 +0000

    adding git pull

--- runextension.sh
+++ runextension.sh
@@ -14,7 +14,8 @@ cd
 rm -R gce-extension/*
 cp /home/sanjaya/stratos-source/extensions/load-balancer/gce-extension/target/org.apache.stratos.gce.extension-4.1.0-SNAPSHOT.zip gce-extension/
 
-cd gce-extention/
+
+cd gce-extension/
 
 unzip org.apache.stratos.gce.extension-4.1.0-SNAPSHOT.zip
 cd org.apache.stratos.gce.extension-4.1.0-SNAPSHOT/bin
